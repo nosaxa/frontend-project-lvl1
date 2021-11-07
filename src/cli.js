1cfcfcb003cd31acf9131bad7cdcf19d0794f5c5
Maxim <nosaxa@protonmail.com>
2021-11-07 19:05:11 +0100

    Make userName variable global

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,13 @@
 import readlineSync from "readline-sync";
 
-export default () => {
-  console.log("Welcome to the Brain Games!");
+const askName = () => {
   const userName = readlineSync.question("May I have your name? ");
+  return userName;
+};
+
+console.log("Welcome to the Brain Games!");
+export const userName = askName();
+
+export default () => {
   console.log(`Hello, ${userName}!`);
 };

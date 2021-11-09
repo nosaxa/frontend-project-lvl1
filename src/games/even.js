88e1dc22fbe19d7926fe11928fc2728c82710bc3
Maxim <nosaxa@protonmail.com>
2021-11-09 21:08:43 +0100

    Rewrite Even-game

---
+++ src/games/even.js
@@ -0,0 +1,14 @@
+import startGame from "../index.js";
+import getRandomNumber from "../helpers.js";
+
+const message = `Answer "yes" if the number is even, otherwise answer "no".`;
+
+const evenGameData = () => {
+  const question = getRandomNumber();
+  const answer = question % 2 === 0 ? "yes" : "no";
+  return [question, answer];
+};
+
+const even = () => startGame(message, evenGameData);
+
+export default even;

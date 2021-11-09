5b27af5d01849d0ce2192c148dd30d6759c9fcd7
Maxim <nosaxa@protonmail.com>
2021-11-09 21:06:23 +0100

    Create new calculator game

---
+++ src/games/calculator.js
@@ -0,0 +1,33 @@
+import startGame from "../index.js";
+import getRandomNumber from "../helpers.js";
+
+const message = "What is the result of the expression?";
+
+const calc = (num1, num2, operator) => {
+  switch (operator) {
+    case "+":
+      return num1 + num2;
+    case "-":
+      return num1 - num2;
+    case "*":
+      return num1 * num2;
+    default:
+      return null;
+  }
+};
+
+const calcGameData = () => {
+  const operators = ["+", "*", "-"];
+  const maxIndex = operators.length - 1;
+  const number1 = getRandomNumber();
+  const number2 = getRandomNumber();
+  const operation = operators[getRandomNumber(0, maxIndex)];
+  const question = `${number1} ${operation} ${number2}`;
+  const answer = String(calc(number1, number2, operation));
+
+  return [question, answer];
+};
+
+const calculator = () => startGame(message, calcGameData);
+
+export default calculator;

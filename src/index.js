9690ce1006e1a4ff05738b72a83dd454acb4faaf
Maxim <nosaxa@protonmail.com>
2021-11-09 21:04:48 +0100

    Write the logic for the all games in one file

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import readlineSync from "readline-sync";
+
+const startGame = (gameMessage, gameData) => {
+  console.log("Welcome to the Brain Games!");
+  const userName = readlineSync.question("May I have your name? ");
+  console.log(`Hello, ${userName}!`);
+  console.log(gameMessage);
+  for (let i = 0; i < 3; i += 1) {
+    const [question, answer] = gameData();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question(`Your answer: `);
+    if (userAnswer === answer) {
+      console.log("Correct!");
+    } else {
+      console.log(
+        `'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.
+Let's try again, ${userName}!`
+      );
+      return;
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export default startGame;

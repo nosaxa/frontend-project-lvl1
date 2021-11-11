7f7a89e31dcce37010c351f73fa686407d8bb1fc
Maxim <nosaxa@protonmail.com>
2021-11-11 12:45:12 +0100

    Create brain-prime game

---
+++ src/games/prime.js
@@ -0,0 +1,23 @@
+import playGame from "../index.js";
+import getRandomNumber from "../helpers.js";
+
+const message = `Answer "yes" if given number is prime. Otherwise answer "no".`;
+
+const primeGameData = () => {
+  const number = getRandomNumber(1, 20);
+  const dividers = [];
+  for (let i = 1; i <= number; i += 1) {
+    if (number % i === 0) {
+      dividers.push(i);
+    }
+  }
+  const isPrime = dividers.length > 2 ? "no" : "yes";
+  const question = number;
+  const answer = isPrime;
+
+  return [question, answer];
+};
+
+const prime = () => playGame(message, primeGameData);
+
+export default prime;

d6a6c3bdbfa95f351665ce4cbd97d4378ac1dede
Maxim <nosaxa@protonmail.com>
2021-11-12 21:47:28 +0100

    Add arguments to getRandomNumber function to make code more clear

--- src/games/calculator.js
+++ src/games/calculator.js
@@ -19,8 +19,8 @@ const calc = (num1, num2, operator) => {
 const calcGameData = () => {
   const operators = ['+', '*', '-'];
   const lastIndex = operators.length - 1;
-  const number1 = getRandomNumber();
-  const number2 = getRandomNumber();
+  const number1 = getRandomNumber(1, 10);
+  const number2 = getRandomNumber(1, 10);
   const operation = operators[getRandomNumber(0, lastIndex)];
   const question = `${number1} ${operation} ${number2}`;
   const answer = String(calc(number1, number2, operation));

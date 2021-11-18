05326a0fe485661b0369afc120ccb422433b5f70
Maxim <nosaxa@protonmail.com>
2021-11-18 08:07:28 +0100

    Format isPrime function

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,7 +5,9 @@ const message = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (number) => {
   for (let i = 2; i < number; i += 1) {
-    if (number % i === 0) return false;
+    if (number % i === 0) {
+      return false;
+    }
   }
   return true;
 };
